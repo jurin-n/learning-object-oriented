10a5423f9b1672a81ca46374da917d3974eeb2fa
jurin <jurin.nakano@gmail.com>
2014-06-28 21:22:18 +0900

    InstrumentのサブクラスとInstrumentSpecのサブクラスを削除。

--- src/headfirst/chap05/Mandolin.java
+++
@@ -1,9 +0,0 @@
-package headfirst.chap05;
-
-public class Mandolin extends Instrument {
-
-	public Mandolin(String serialNumber,double price,
-			MandolinSpec spec){
-		super(serialNumber,price,spec);
-	}
-}
