43bc7cbb12335d23cdfb86833bf43c8cdafc2231
jurin <jurin.nakano@gmail.com>
2014-06-28 21:34:03 +0900

    InstrumentとInstrumentSpecの変更結果を Inventoryに反映。

--- src/headfirst/chap05/Instrument.java
+++ src/headfirst/chap05/Instrument.java
@@ -1,27 +1,19 @@
 package headfirst.chap05;
 
 public class Instrument {
-	private InstrumentType instrumentType;
 	private String serialNumber;
 	private double price;
 	private InstrumentSpec spec;
 	
 	public Instrument(
-			 InstrumentType instrumentType
-			,String serialNumber
+			 String serialNumber
 			,double price
 			,InstrumentSpec spec){
-
-		this.instrumentType=instrumentType;
 		this.serialNumber=serialNumber;
 		this.price=price;
 		this.spec=spec;
 	}
 	
-	public InstrumentType getInstrumentType() {
-		return instrumentType;
-	}
-
 	public String getSerialNumber() {
 		return serialNumber;
 	}
